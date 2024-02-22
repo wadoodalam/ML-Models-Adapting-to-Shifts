be00566d7b7ff9009e794ca29219f2668b5ee6fa
wadoodalam <wadoodalam@outlook.com>
2024-02-21 20:18:25 -0800

    Predict, and display in table without BBSC

--- shift_adapt.py
+++ shift_adapt.py
@@ -5,13 +5,25 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score
-def LoadData(path,x_cols,y_cols):
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def LoadData(path):
     # Load data from the path
-    data = pd.read_csv(path)
+    data = pd.read_csv(path,usecols=['Distance(mi)', 'Temperature(F)', 'Wind_Chill(F)', 'Humidity(%)',
+                     'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)', 'Precipitation(in)',
+                     'Amenity', 'Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit',
+                     'Railway', 'Roundabout', 'Station', 'Stop', 'Traffic_Calming',
+                     'Traffic_Signal', 'Turning_Loop', 'Severity'])
     # Assign X data
-    X = data[x_cols]
+    X = data[['Distance(mi)', 'Temperature(F)', 'Wind_Chill(F)', 'Humidity(%)',
+                     'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)', 'Precipitation(in)',
+                     'Amenity', 'Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit',
+                     'Railway', 'Roundabout', 'Station', 'Stop', 'Traffic_Calming',
+                     'Traffic_Signal', 'Turning_Loop']]
     # Assign Y data
-    Y = data[y_cols]
+    Y = data['Severity']
     return X,Y
 
 def TrainModel(X,Y,classifier):
@@ -22,11 +34,11 @@ def TrainModel(X,Y,classifier):
 def Predict(X,Y,classifier):
     #  predict from the model
     Y_predict = classifier.predict(X)
-    # Calculate accuracy
-    accuracy = accuracy_score(Y,Y_predict)
+    # Calculate accuracy in % with 2 decimal places
+    accuracy = round(accuracy_score(Y, Y_predict) * 100, 2)
     return accuracy
 
-def TrainPredict(X_train,Y_train,X,Y):
+def Train(X_train,Y_train):
     
     # Train all the classifiers
     Dummy_maj = TrainModel(X_train,Y_train,DummyClassifier(strategy='most_frequent',random_state=0))
@@ -35,16 +47,69 @@ def TrainPredict(X_train,Y_train,X,Y):
     ThreeNN = TrainModel(X_train,Y_train,KNeighborsClassifier(n_neighbors=3))
     NineNN = TrainModel(X_train,Y_train,KNeighborsClassifier(n_neighbors=9))
     GP = TrainModel(X_train,Y_train,GaussianProcessClassifier(random_state=0))
+    # assign the trained models to a list to return
+    classifiers = [Dummy_maj, Dummy_strat,RF,GP,ThreeNN,NineNN]
+    return classifiers
+
+def ConvertToDataFrameWithModel(val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies):
+    model_names = ['Dummy Classifier with Most-Frequent', 'Dummy Classifier with Stratified', 'Random Forest', 
+                   'GaussianProcess','3-NearestNeighbor', '9-NearestNeighbor']
+    df = pd.DataFrame({'Classifier': model_names, 'Val-TX':val_accuracies, 'Test1-TX':t1_accuracies,
+                       'Test2-FL':t2_accuracies, 'Test3-FL':t3_accuracies})
+    return df
+
+def PredictAccuracies(classifiers,X_val,Y_val,X_t1,Y_t1,X_t2,Y_t2,X_t3,Y_t3):
+    # Init list for accuracies
+    val_accuracies = []
+    t1_accuracies = []
+    t2_accuracies = []
+    t3_accuracies = []
+    
+    # Predict and get the accuracy on val set for all models
+    for model in classifiers:
+        # Predict, using .values to predict using the values, otherwise gives error
+        accuracy = Predict(X_val.values,Y_val.values,model)
+        val_accuracies.append(accuracy)
+        
+    # Predict and get the accuracy on test set 1 for all models
+    for model in classifiers:
+        # Predict, using .values to predict using the values, otherwise gives error
+        accuracy = Predict(X_t1.values,Y_t1.values,model)
+        t1_accuracies.append(accuracy)  
+    
+    # Predict and get the accuracy on test set 2 for all models
+    for model in classifiers:
+        # Predict, using .values to predict using the values, otherwise gives error
+        accuracy = Predict(X_t2.values,Y_t2.values,model)
+        t2_accuracies.append(accuracy)   
+        
+    # Predict and get the accuracy on test set 3 for all models
+    for model in classifiers:
+        # Predict, using .values to predict using the values, otherwise gives error
+        accuracy = Predict(X_t3.values,Y_t3.values,model)
+        t3_accuracies.append(accuracy)   
+    return val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies
 
 
 if __name__ == "__main__":
-    # Features for X and Y
-    X_cols = ['Distance(mi)', 'Temperature(F)', 'Wind_Chill(F)', 'Humidity(%)',
-                     'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)', 'Precipitation(in)',
-                     'Amenity', 'Bump', 'Crossing', 'Give_Way', 'Junction', 'No_Exit',
-                     'Railway', 'Roundabout', 'Station', 'Stop', 'Traffic_Calming',
-                     'Traffic_Signal', 'Turning_Loop']
-    Y_cols = ['Severity']
+    
     # Load training data
-    X_train,Y_train = LoadData('/Users/wadoodalam/ML Models Adapting to Shifts/train-TX.csv',X_cols,Y_cols)
- 
+    X_train,Y_train = LoadData('/Users/wadoodalam/ML Models Adapting to Shifts/train-TX.csv')
+    # Load val data
+    X_val,Y_val = LoadData('/Users/wadoodalam/ML Models Adapting to Shifts/val-TX.csv')
+    # Load test 1 data
+    X_t1,Y_t1 = LoadData('/Users/wadoodalam/ML Models Adapting to Shifts/test1-TX.csv')
+    # Load test 2 data
+    X_t2,Y_t2 = LoadData('/Users/wadoodalam/ML Models Adapting to Shifts/test2-FL.csv')
+    # Load test 3 data
+    X_t3,Y_t3 = LoadData('/Users/wadoodalam/ML Models Adapting to Shifts/test3-FL.csv')
+    
+    # Train all the classifiers
+    classifiers = Train(X_train,Y_train)
+    
+    # Get all accuracies without BBSC
+    val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies = PredictAccuracies(classifiers,X_val,Y_val,X_t1,Y_t1,X_t2,Y_t2,X_t3,Y_t3)        
+    
+    
+    df = ConvertToDataFrameWithModel(val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies)
+    print(df)
