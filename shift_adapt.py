49c12501ccb056e54dcd8307e2ab44a11f86231e
wadoodalam <wadoodalam@outlook.com>
2024-02-23 01:12:52 -0800

    Predict with BBSC update prob

--- shift_adapt.py
+++ shift_adapt.py
@@ -32,12 +32,18 @@ def TrainModel(X,Y,classifier):
     classifier.fit(X,Y)
     return classifier
 
-def Predict(X,Y,classifier):
+def Predict(X,Y,classifier,prob=False):
     #  predict from the model
-    Y_predict = classifier.predict(X)
+    Y_predict = classifier.predict(X.values)
+    # Get predicted probabilities 
+    Y_prob = classifier.predict_proba(X.values)
     # Calculate accuracy in % with 2 decimal places
     accuracy = round(accuracy_score(Y, Y_predict) * 100, 2)
-    return accuracy, Y_predict
+    # Return predicted prob if asked
+    if prob:
+        return Y_predict, Y_prob
+    else:
+        return accuracy, Y_predict
 
 def Train(X_train,Y_train):
     
@@ -54,9 +60,16 @@ def Train(X_train,Y_train):
 
 def ConvertAccuracyTable(val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies):
     model_names = ['Dummy Classifier with Most-Frequent', 'Dummy Classifier with Stratified', 'Random Forest', 
-                   'GaussianProcess','3-NearestNeighbor', '9-NearestNeighbor']
+                'GaussianProcess','3-NearestNeighbor', '9-NearestNeighbor']
+        
     df = pd.DataFrame({'Classifier': model_names, 'Val-TX':val_accuracies, 'Test1-TX':t1_accuracies,
-                       'Test2-FL':t2_accuracies, 'Test3-FL':t3_accuracies})
+                    'Test2-FL':t2_accuracies, 'Test3-FL':t3_accuracies})
+    return df
+
+def ConvertAccuracyTableBBSC(t1_accuracies,t2_accuracies,t3_accuracies):
+    model_names = ['Random Forest','GaussianProcess','3-NearestNeighbor', '9-NearestNeighbor']    
+    df = pd.DataFrame({'Classifier': model_names,'Test1-TX':t1_accuracies,'Test2-FL':t2_accuracies, 'Test3-FL':t3_accuracies})
+    
     return df
 
 def PredictAccuracies(classifiers,X_val,Y_val,X_t1,Y_t1,X_t2,Y_t2,X_t3,Y_t3):
@@ -70,33 +83,33 @@ def PredictAccuracies(classifiers,X_val,Y_val,X_t1,Y_t1,X_t2,Y_t2,X_t3,Y_t3):
     # Predict and get the accuracy on val set for all models
     for model in classifiers:
         # Predict, using .values to predict using the values, otherwise gives error
-        accuracy,Y_val_predict_var = Predict(X_val.values,Y_val.values,model)
+        accuracy,Y_val_predict_var = Predict(X_val,Y_val,model)
         val_accuracies.append(accuracy)
         
     # Predict and get the accuracy on test set 1 for all models
 
         # Predict, using .values to predict using the values, otherwise gives error
-        accuracy,Y_t1_predict_var = Predict(X_t1.values,Y_t1.values,model)
+        accuracy,Y_t1_predict_var = Predict(X_t1,Y_t1,model)
         
         t1_accuracies.append(accuracy)  
     
     # Predict and get the accuracy on test set 2 for all models
 
         # Predict, using .values to predict using the values, otherwise gives error
-        accuracy,Y_t2_predict_var = Predict(X_t2.values,Y_t2.values,model)
+        accuracy,Y_t2_predict_var = Predict(X_t2,Y_t2,model)
         
         t2_accuracies.append(accuracy)   
         
     # Predict and get the accuracy on test set 3 for all models
         # Predict, using .values to predict using the values, otherwise gives error
-        accuracy,Y_t3_predict_var = Predict(X_t3.values,Y_t3.values,model)
+        accuracy,Y_t3_predict_var = Predict(X_t3,Y_t3,model)
         
         t3_accuracies.append(accuracy)  
         if i > 1 and i < len(classifiers):
-            weights_adapted[model] = [] 
-            weights_adapted[model].extend([float(val) for val in analyze_val_data(Y_val,Y_val_predict_var,Y_t1_predict_var)])
-            weights_adapted[model].extend([float(val) for val in analyze_val_data(Y_val,Y_val_predict_var,Y_t2_predict_var)])
-            weights_adapted[model].extend([float(val) for val in analyze_val_data(Y_val,Y_val_predict_var,Y_t3_predict_var)])
+            weights_adapted[str(model)] = [] 
+            weights_adapted[str(model)].extend([float(val) for val in analyze_val_data(Y_val,Y_val_predict_var,Y_t1_predict_var)])
+            weights_adapted[str(model)].extend([float(val) for val in analyze_val_data(Y_val,Y_val_predict_var,Y_t2_predict_var)])
+            weights_adapted[str(model)].extend([float(val) for val in analyze_val_data(Y_val,Y_val_predict_var,Y_t3_predict_var)])
                 
         i+=1 
     return val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies,weights_adapted
@@ -118,7 +131,7 @@ def ConvertWeightsToTable(weights):
                 # slice the 4 corresponding values
                 col_values = values[i*4:(i+1)*4]
                 # Round each value to 2 decimal places
-                col_values_rounded = [round(value, 2) for value in col_values]
+                col_values_rounded = ["{:.2f}".format(value) for value in col_values]
                 row[col_name] = col_values_rounded
             data.append(row)
         
@@ -127,9 +140,106 @@ def ConvertWeightsToTable(weights):
     df = pd.DataFrame(data)
     return df
 
+def SeparateWights(weights):
+    # Init Dict for weight separation
+    RF_weights = {'T1': [], 'T2': [], 'T3': []}
+    GP_weights = {'T1': [], 'T2': [], 'T3': []}
+    ThreeNN_weights = {'T1': [], 'T2': [], 'T3': []}
+    NineNN_weights = {'T1': [], 'T2': [], 'T3': []}
+    
+    # Separate weights for for each classifier per test set
+    RF_weights['T1'] = (weights['RandomForestClassifier(min_samples_split=10, random_state=0)'][:4])
+    RF_weights['T2'] = (weights['RandomForestClassifier(min_samples_split=10, random_state=0)'][4:8])
+    RF_weights['T3'] = (weights['RandomForestClassifier(min_samples_split=10, random_state=0)'][8:12])
+    
+    GP_weights['T1'] = (weights['GaussianProcessClassifier(random_state=0)'][:4])
+    GP_weights['T2'] = (weights['GaussianProcessClassifier(random_state=0)'][4:8])
+    GP_weights['T3'] = (weights['GaussianProcessClassifier(random_state=0)'][8:12])
+    
+    ThreeNN_weights['T1'] = (weights['KNeighborsClassifier(n_neighbors=3)'][:4])
+    ThreeNN_weights['T2'] = (weights['KNeighborsClassifier(n_neighbors=3)'][4:8])
+    ThreeNN_weights['T3'] = (weights['KNeighborsClassifier(n_neighbors=3)'][8:12])
+    
+    NineNN_weights['T1'] = (weights['KNeighborsClassifier(n_neighbors=9)'][:4])
+    NineNN_weights['T2'] = (weights['KNeighborsClassifier(n_neighbors=9)'][4:8])
+    NineNN_weights['T3'] = (weights['KNeighborsClassifier(n_neighbors=9)'][8:12])
 
+    # Return weights dicts
+    return RF_weights,GP_weights,ThreeNN_weights,NineNN_weights
 
+def UniqueClasses(Y):
+    classes = np.unique(Y).tolist()
+    return classes
 
+def PredictAccuraciesBBSC(classifiers,X_val,Y_val,X_t1,Y_t1,X_t2,Y_t2,X_t3,Y_t3,weights):
+    # slice to ignore dummy classifiers
+    classifiers = classifiers[2:len(classifiers)]
+    
+    
+    # Get Prob and Pred for each model for each test set
+    Y_t1_Predict_RF, Y_t1_Prob_RF = Predict(X_t1,Y_t1,classifiers[0],True)
+    Y_t2_Predict_RF, Y_t2_Prob_RF = Predict(X_t2,Y_t2,classifiers[0],True)
+    Y_t3_Predict_RF, Y_t3_Prob_RF = Predict(X_t3,Y_t3,classifiers[0],True)
+    
+    Y_t1_Predict_GP, Y_t1_Prob_GP = Predict(X_t1,Y_t1,classifiers[1],True)
+    Y_t2_Predict_GP, Y_t2_Prob_GP = Predict(X_t2,Y_t2,classifiers[1],True)
+    Y_t3_Predict_GP, Y_t3_Prob_GP = Predict(X_t3,Y_t3,classifiers[1],True)
+    
+    Y_t1_Predict_3NN, Y_t1_Prob_3NN = Predict(X_t1,Y_t1,classifiers[2],True)
+    Y_t2_Predict_3NN, Y_t2_Prob_3NN = Predict(X_t2,Y_t2,classifiers[2],True)
+    Y_t3_Predict_3NN, Y_t3_Prob_3NN = Predict(X_t3,Y_t3,classifiers[2],True)
+    
+    Y_t1_Predict_9NN, Y_t1_Prob_9NN = Predict(X_t1,Y_t1,classifiers[3],True)
+    Y_t2_Predict_9NN, Y_t2_Prob_9NN = Predict(X_t2,Y_t2,classifiers[3],True)
+    Y_t3_Predict_9NN, Y_t3_Prob_9NN = Predict(X_t3,Y_t3,classifiers[3],True)
+     
+    # Get unique classes (same for all test sets), hence only getting from 1 test set
+    unique_classes = UniqueClasses(Y_t1)
+    
+    # Get all the weights, dicts, divided by models and test sets
+    RF_weights,GP_weights,ThreeNN_weights,NineNN_weights = SeparateWights(weights)
+    
+    # Update the prob using BBSC
+    new_t1_pred_RF, new_t1_prob_RF = update_probs(unique_classes,RF_weights['T1'],Y_t1_Predict_RF,Y_t1_Prob_RF)
+    new_t2_pred_RF, new_t2_prob_RF = update_probs(unique_classes,RF_weights['T2'],Y_t2_Predict_RF,Y_t2_Prob_RF)
+    new_t3_pred_RF, new_t3_prob_RF = update_probs(unique_classes,RF_weights['T3'],Y_t3_Predict_RF,Y_t3_Prob_RF)
+    
+    new_t1_pred_GP, new_t1_prob_GP = update_probs(unique_classes,GP_weights['T1'],Y_t1_Predict_GP,Y_t1_Prob_GP)
+    new_t2_pred_GP, new_t2_prob_GP = update_probs(unique_classes,GP_weights['T2'],Y_t2_Predict_GP,Y_t2_Prob_GP)
+    new_t3_pred_GP, new_t3_prob_GP = update_probs(unique_classes,GP_weights['T3'],Y_t3_Predict_GP,Y_t3_Prob_GP)
+
+    new_t1_pred_3NN, new_t1_prob_3NN = update_probs(unique_classes,ThreeNN_weights['T1'],Y_t1_Predict_3NN,Y_t1_Prob_3NN)
+    new_t2_pred_3NN, new_t2_prob_3NN = update_probs(unique_classes,ThreeNN_weights['T2'],Y_t2_Predict_3NN,Y_t2_Prob_3NN)
+    new_t3_pred_3NN, new_t3_prob_3NN = update_probs(unique_classes,ThreeNN_weights['T3'],Y_t3_Predict_3NN,Y_t3_Prob_3NN)
+
+    new_t1_pred_9NN, new_t1_prob_9NN = update_probs(unique_classes,NineNN_weights['T1'],Y_t1_Predict_9NN,Y_t1_Prob_9NN)
+    new_t2_pred_9NN, new_t2_prob_9NN = update_probs(unique_classes,NineNN_weights['T2'],Y_t2_Predict_9NN,Y_t2_Prob_9NN)
+    new_t3_pred_9NN, new_t3_prob_9NN = update_probs(unique_classes,NineNN_weights['T3'],Y_t3_Predict_9NN,Y_t3_Prob_9NN)
+
+    # Append all the accuracies in the lists for each test set
+    # The indices represent the classifiers by RF,GP,3NN,9NN respectively
+    t1_acc_BBSC = []
+    t2_acc_BBSC = []
+    t3_acc_BBSC = []
+    
+    t1_acc_BBSC.append(round(accuracy_score(Y_t1, new_t1_pred_RF) * 100, 2))
+    t1_acc_BBSC.append(round(accuracy_score(Y_t1, new_t1_pred_GP) * 100, 2))
+    t1_acc_BBSC.append(round(accuracy_score(Y_t1, new_t1_pred_3NN) * 100, 2))
+    t1_acc_BBSC.append(round(accuracy_score(Y_t1, new_t1_pred_9NN) * 100, 2))
+    
+    t2_acc_BBSC.append(round(accuracy_score(Y_t2, new_t2_pred_RF) * 100, 2))
+    t2_acc_BBSC.append(round(accuracy_score(Y_t2, new_t2_pred_GP) * 100, 2))
+    t2_acc_BBSC.append(round(accuracy_score(Y_t2, new_t2_pred_3NN) * 100, 2))
+    t2_acc_BBSC.append(round(accuracy_score(Y_t2, new_t2_pred_9NN) * 100, 2))
+    
+    t3_acc_BBSC.append(round(accuracy_score(Y_t3, new_t3_pred_RF) * 100, 2))
+    t3_acc_BBSC.append(round(accuracy_score(Y_t3, new_t3_pred_GP) * 100, 2))
+    t3_acc_BBSC.append(round(accuracy_score(Y_t3, new_t3_pred_3NN) * 100, 2))
+    t3_acc_BBSC.append(round(accuracy_score(Y_t3, new_t3_pred_9NN) * 100, 2))
+    
+    return t1_acc_BBSC, t2_acc_BBSC, t3_acc_BBSC
+    
+    
 if __name__ == "__main__":
     
     # Load training data
@@ -150,12 +260,17 @@ if __name__ == "__main__":
     # Get all accuracies without BBSC
     val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies,weights = PredictAccuracies(classifiers,X_val,Y_val,X_t1,Y_t1,X_t2,Y_t2,X_t3,Y_t3)        
     
+    # Get all accuracies with BBSC
+    t1_acc_BBSC, t2_acc_BBSC, t3_acc_BBSC = PredictAccuraciesBBSC(classifiers,X_val,Y_val,X_t1,Y_t1,X_t2,Y_t2,X_t3,Y_t3,weights)
     
-    # Convert accuracies to dataframe and output in a csv table to accuracy.csv
+    # Convert accuracies and weights to dataframe 
     df_accuracy = ConvertAccuracyTable(val_accuracies,t1_accuracies,t2_accuracies,t3_accuracies)
-    df_accuracy.to_csv('accuracy.csv')
-    
-    # Convert weights to dataframe and output in a csv table to weights.csv
     df_weight = ConvertWeightsToTable(weights)
-    df_weight.to_csv('weights.csv')
+    df_accuracy_BBSC = ConvertAccuracyTableBBSC(t1_acc_BBSC,t2_acc_BBSC,t3_acc_BBSC)
     
+    # Uncomment to get csv outputs for both accuracies and weights table
+    #df_accuracy.to_csv('accuracy.csv')
+    #df_weight.to_csv('weights.csv')
+    #df_accuracy_BBSC.to_csv('accuracy_BBSC.csv')
+    
+
